0f75fda225a76c4b7f562780e348abb4439ea109
DPCBackup <memeshaimamu@gmail.com>
2026-04-19 21:24:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,7 +30,7 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     try:
-        stick_id = "CAACAgUAAxkBAAEQJmJpViid_0yscWKPfh3RMCY8pIkmXwACMAcAAqzbsFexyKU6FPQAAjgE"
+        stick_id = "CAACAgQAAxkBAAEQ9Fxp5PmbCw7MlRMKQCM6QwlfzYOVYwACkBUAA3mRUZGx4GwLX9XCOwQ"
         try:
             sticker = await message.reply_sticker(sticker=stick_id)
         except Exception as e:

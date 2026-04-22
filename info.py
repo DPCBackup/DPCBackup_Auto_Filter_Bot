ecd33785b7b03fa01c51355cd987e8ba26f9f098
DPCBackup <memeshaimamu@gmail.com>
2026-04-22 21:09:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ FSUB_PICS = (environ.get('FSUB_PICS', 'https://ibb.co/chRtcwTK https://ibb.co/zV
 # ============================
 # Admin, Channels & Users Configuration
 # ============================
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8759957086 7638788211').split()] # Replace with the actual admin ID(s) to add
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8759957086 7638788211 8397763468').split()] # Replace with the actual admin ID(s) to add
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002334064264 -1003432455254 -1003684244916').split()]  # Channel id for auto indexing (make sure bot is admin)
 
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003244820346'))  # Log channel id (make sure bot is admin)
